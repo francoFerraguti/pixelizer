7655f6c945a3bf0e76169a68d3164f7a3de8e3e4
Franco Ferra <Franco Ferra>
2017-10-30 17:45:00 -0300

    Fixed algorithm

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"flag"
+	"github.com/liteByte/frango"
 	"image"
 	"image/color"
+	"image/draw"
 	_ "image/jpeg"
 	"image/png"
 	"log"
@@ -11,13 +14,22 @@ import (
 )
 
 func main() {
+	fileNameFlag := flag.String("filename", "sample.png", "")
+	pixelWidthFlag := flag.Int("pixelWidth", 2, "")
+	pixelHeightFlag := flag.Int("pixelHeight", 2, "")
+	flag.Parse()
+
+	frango.SeedRandom()
+
 	createSampleImage("sample.png", 100, 100)
 
-	sampleImage := loadImage("gioconda_2.jpg")
+	sampleImage := loadImage(*fileNameFlag)
 
-	pixelizedImage := pixelizeImage(sampleImage, 4, 6)
+	pixelizedImage := pixelizeImage(sampleImage, *pixelWidthFlag, *pixelHeightFlag)
 
-	outputFile, err := os.Create("output.png")
+	outputName := "output/" + frango.GetRandomString(8) + "_" + strconv.Itoa(*pixelWidthFlag) + "_" + strconv.Itoa(*pixelHeightFlag) + ".png"
+
+	outputFile, err := os.Create(outputName)
 	defer outputFile.Close()
 
 	if err != nil {
@@ -28,47 +40,52 @@ func main() {
 }
 
 func pixelizeImage(img image.Image, pixelWidth, pixelHeight int) image.Image {
-	width := img.Bounds().Max.X
-	height := img.Bounds().Max.Y
 
-	widthInFakePixels := width / pixelWidth
-	heightInFakePixels := height / pixelHeight
+	width, height, widthInFakePixels, heightInFakePixels := getImageData(img, pixelWidth, pixelHeight)
 
+	log.Println("Original image dimensions: " + strconv.Itoa(width) + "x" + strconv.Itoa(height))
 	log.Println("Width in fake pixels: " + strconv.Itoa(widthInFakePixels))
 	log.Println("Height in fake pixels: " + strconv.Itoa(heightInFakePixels))
 
-	rArray, gArray, bArray, aArray := createRGBAArrays(widthInFakePixels, heightInFakePixels)
+	rArray, gArray, bArray, aArray := createRGBAArrays(widthInFakePixels, heightInFakePixels) //These are 4 arrays with the size of [widthInFaxePixels, heightInFakePixels]
 
-	for y := 0; y < height; y++ {
+	for y := 0; y < height; y++ { //This double loop adds the color of every pixel to the portion of the fakePixelArray that we created before
 		for x := 0; x < width; x++ {
 
 			fakePixelX := 0
 			fakePixelY := 0
+			c := 0
 
-			r, g, b, a := img.At(x, y).RGBA()
+			r, g, b, a := img.At(x, y).RGBA() //Gets the color of the current pixel
 
-			for i := 0; i < width; i += pixelWidth {
+			for i := 0; i <= width; i += pixelWidth {
+				c++
+
 				if x < i {
-					fakePixelX = i / pixelWidth
+					fakePixelX = c - 2 //Assigns the correct fakePixel position to the pixel
 					break
 				}
 			}
 
-			for i := 0; i < height; i += pixelHeight {
+			c = 0
+
+			for i := 0; i <= height; i += pixelHeight {
+				c++
+
 				if y < i {
-					fakePixelY = i / pixelHeight
+					fakePixelY = c - 2 //Assigns the correct fakePixel position to the pixel
 					break
 				}
 			}
 
-			rArray[fakePixelX][fakePixelY] += int(r / 0x101)
-			gArray[fakePixelX][fakePixelY] += int(g / 0x101)
-			bArray[fakePixelX][fakePixelY] += int(b / 0x101)
-			aArray[fakePixelX][fakePixelY] += int(a / 0x101)
+			rArray[fakePixelX][fakePixelY] += int(r / 0x101) //Adds the color
+			gArray[fakePixelX][fakePixelY] += int(g / 0x101) //Adds the color
+			bArray[fakePixelX][fakePixelY] += int(b / 0x101) //Adds the color
+			aArray[fakePixelX][fakePixelY] += int(a / 0x101) //Adds the color
 		}
 	}
 
-	for y := 0; y < heightInFakePixels; y++ {
+	for y := 0; y < heightInFakePixels; y++ { //This double loop gets the average of the colors we just added
 		for x := 0; x < widthInFakePixels; x++ {
 			rArray[x][y] = rArray[x][y] / pixelWidth / pixelHeight
 			gArray[x][y] = gArray[x][y] / pixelWidth / pixelHeight
@@ -77,24 +94,33 @@ func pixelizeImage(img image.Image, pixelWidth, pixelHeight int) image.Image {
 		}
 	}
 
-	finalImage := image.NewRGBA(image.Rect(0, 0, width, height))
+	log.Println("Finished creating the fakePixel arrays")
 
-	for y := 0; y < height; y++ {
+	finalImage := image.NewRGBA(image.Rect(0, 0, width, height)) //Creates an empty image to paste the new one above
+
+	for y := 0; y < height; y++ { //It's the same double loop as the one above, but this time it pastes the fakePixel color onto a new image
 		for x := 0; x < width; x++ {
 
 			fakePixelX := 0
 			fakePixelY := 0
+			c := 0
 
-			for i := 0; i < width; i += pixelWidth {
+			for i := 0; i <= width; i += pixelWidth {
+				c++
+
 				if x < i {
-					fakePixelX = i / pixelWidth
+					fakePixelX = c - 2
 					break
 				}
 			}
 
-			for i := 0; i < height; i += pixelHeight {
+			c = 0
+
+			for i := 0; i <= height; i += pixelHeight {
+				c++
+
 				if y < i {
-					fakePixelY = i / pixelHeight
+					fakePixelY = c - 2
 					break
 				}
 			}
@@ -104,28 +130,55 @@ func pixelizeImage(img image.Image, pixelWidth, pixelHeight int) image.Image {
 		}
 	}
 
+	log.Println("Finished pixelizing image")
+
 	return finalImage
 }
 
+func getImageData(img image.Image, pixelWidth, pixelHeight int) (int, int, int, int) {
+	width := img.Bounds().Max.X
+	height := img.Bounds().Max.Y
+
+	for width%pixelWidth != 0 {
+		width--
+	}
+
+	for height%pixelHeight != 0 {
+		height--
+	}
+
+	widthInFakePixels := width / pixelWidth
+	heightInFakePixels := height / pixelHeight
+
+	return width, height, widthInFakePixels, heightInFakePixels
+}
+
+func convertImageToRGBA(img image.Image) image.RGBA {
+	bounds := img.Bounds()
+	imageRGBA := image.NewRGBA(image.Rect(0, 0, bounds.Dx(), bounds.Dy()))
+	draw.Draw(imageRGBA, imageRGBA.Bounds(), img, bounds.Min, draw.Src)
+	return *imageRGBA
+}
+
 func createRGBAArrays(width, height int) ([][]int, [][]int, [][]int, [][]int) {
-	rArray := make([][]int, height)
+	rArray := make([][]int, width)
 	for i := range rArray {
-		rArray[i] = make([]int, width)
+		rArray[i] = make([]int, height)
 	}
 
-	gArray := make([][]int, height)
+	gArray := make([][]int, width)
 	for i := range gArray {
-		gArray[i] = make([]int, width)
+		gArray[i] = make([]int, height)
 	}
 
-	bArray := make([][]int, height)
+	bArray := make([][]int, width)
 	for i := range bArray {
-		bArray[i] = make([]int, width)
+		bArray[i] = make([]int, height)
 	}
 
-	aArray := make([][]int, height)
+	aArray := make([][]int, width)
 	for i := range aArray {
-		aArray[i] = make([]int, width)
+		aArray[i] = make([]int, height)
 	}
 
 	return rArray, gArray, bArray, aArray
